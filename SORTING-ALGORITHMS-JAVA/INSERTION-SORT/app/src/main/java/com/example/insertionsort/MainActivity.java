cc5b4053b7d57abeacd3305e47b7706038727370
Engr. Max Angelo Perin <perinmaks@outlook.ph>
2020-02-04 11:59:42 +0800

    UPDATED BY: SIR PERIN
    Input array using: 1,2,3,4,5,1
    Then split string to integers, then sort then concatenate integers to string.

--- SORTING-ALGORITHMS-JAVA/INSERTION-SORT/app/src/main/java/com/example/insertionsort/MainActivity.java
+++ SORTING-ALGORITHMS-JAVA/INSERTION-SORT/app/src/main/java/com/example/insertionsort/MainActivity.java
@@ -1,48 +1,76 @@
 package com.example.insertionsort;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Arrays;
+import androidx.appcompat.app.AppCompatActivity;
+
 
 
 public class MainActivity extends AppCompatActivity {
 
     int arraySize = 0;
-    int[] arrayNum = new int[99];
+    int[] arrayNum = new int[20];
     int temp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button sortBtn = (Button) findViewById(R.id.sortButton);
+        //Button sortBtn = (Button) findViewById(R.id.sortButton);
         Button nextBtn = (Button) findViewById(R.id.nextbutton);
 
-            nextBtn.setOnClickListener(new View.OnClickListener() {
+
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     TextView result = (TextView) findViewById(R.id.resultTextView);
-                    EditText inputNum = (EditText) findViewById(R.id.inputNumText);
+                    EditText inputNum = (EditText) findViewById(R.id.inputNumText);   // GIKUHA ANG STRING GIKAN SA EditText
+                    String[] array = inputNum.getText().toString().trim().split(","); /*for a user input of 1,2,3,4 split will return a string array = {"1","2","3","4"} */
+                    // result.setText(array[1]+"");
 
+                    int i;
+                    int[] myInts = new int[20];   // GIHIMONG INTEGER ANG VALUES
+                    for ( i=0; i < array.length; i++) {
+                        myInts[i] = Integer.parseInt(array[i]);
+                    }
 
-                    arrayNum[arraySize] = Integer.parseInt(inputNum.getText().toString());
-                    inputNum.setText(" ");
+                    int[] myInts_sort = new int[20];
+                    MainActivity ob = new MainActivity();
+                    myInts_sort = ob.insertion_sort(myInts);
 
-                    result.setText(arrayNum[arraySize]  + "");
-                    arraySize++;
+                    String result1 = Arrays.toString(myInts_sort);
+
+                    result.setText(result1+"");
+
 
                 }
             }
             );
-
-
-
-
-
     }
+
+    int [] insertion_sort(int[] arr)
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            /* Move elements of arr[0..i-1], that are
+               greater than key, to one position ahead
+               of their current position */
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
+
 }
